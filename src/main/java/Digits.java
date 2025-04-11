7cad5bd9b71c3cc72127cb223112e9816280985b
derricckk11 <132044722+derricckk11@users.noreply.github.com>
2025-04-10 23:25:56 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -26,7 +26,7 @@ public class Digits
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 
-		for(int i = 0; i < digitList.size() -1){
+		for(int i = 0; i < digitList.size() -1; i++){
 
 			if(digitList.get(i).compareTo(digitList.get(i + 1)) >= 0){
 
